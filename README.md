f1b5b1cfc5d84c689d10e6c5b54a39e5bfa9b676
Jiaen Pan <jiaen.m.pan@gmail.com>
2020-06-10 19:34:10 +0200

    actualizado README y terminado los endpoints y la conexion con MongoDB

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # k-api
+
+## Rest api
+
+Este proyecto usa Node.js y MongoDB. Tiene un solo endpoint `/notas`
+
+Para la creacion del endpoint se ha usado Express, con los siguientes métodos http:
+
+### Get
+
+- Para obtener informacion, devuelve todas las notas si no se pasa ningun parametro.
+
+- En el caso de que se desee consultar una sola nota, se tiene que pasar en el query `id:<id del objeto deseado>`, o puede añadir el query al final del link por ejemplo `http://localhost:3000/notas?id=<id del objeto deseado>`
+
+- En el caso de que se desee consultar todas las notas marcadas como favoritas, se tiene que pasar en el query `fav: true`, o `http://localhost:3000/notas?fav=true`
+
+### Post
+
+- Para crear notas, se manda en formato JSON, tiene que tener la siguiente estructura `{ "notas":[ {"nota":"<texto>"}, {"nota": "<texto>"}, ...] }`
+
+### Put
+
+- Para marcar una nota como favorita solo hay que pasar el id de la nota por el metodo put como parametro `id:<numero de id del objeto deseado>` o `http://localhost:3000/notas?id=<id del objeto deseado>`
+
+## instruciones
+
+Antes de empezar, el proyecto asume que existe una base de datos de MongoDB en la maquina local en el puerto predefinido con credenciales predefinidos. Puede modificar la url en el archivo index.ts en la linea 11 sustituyendo la url por una válida.
+
+- `git clone "<url del proyecto>"` para clonarlo
+- `npm install` en la raiz del a carpeta
+- `tsc *.ts` para compilar los archivos ts
+- `node index.js` para inicializar el servidor en el puerto 3000
---
+++ index.ts
@@ -0,0 +1,77 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+mongoose.connect("mongodb://localhost:27017/muro", {useNewUrlParser: true, useUnifiedTopology: true})
+
+const notaSchema = mongoose.Schema({
+    nota: String,
+    favorito: Boolean
+})
+const Nota = mongoose.model('Nota', notaSchema)
+
+app.route("/notas")
+.post((req,res) => {
+    let notas = req.body.notas
+    if(notas){
+        notas.forEach(note => {
+            let newNota = new Nota({
+                nota: note.nota,
+                favorito: false
+            })
+            newNota.save()
+        });
+        res.sendStatus(201)
+    } else {
+        res.sendStatus(400)
+    }
+})
+.get((req,res) => {
+    let id = req.query.id
+    let fav = req.query.fav
+    if (id){
+        Nota.findById(id, (err, data) => {
+            if (data) {
+                res.send(data)
+            } else {
+                res.sendStatus(404)
+            }
+        })
+    } else if (fav === 'true') {
+        Nota.find({favorito: true},(err,data) => {
+            if (data){
+                res.send(data)
+            } else {
+                res.sendStatus(404)
+            }
+        }) 
+    } else {
+        Nota.find({}, (err, data) => {
+            if(!err){
+                res.send(data)
+            } else {
+                res.sendStatus(500)
+            }
+        })
+    }
+})
+.put((req,res) => {
+    let id = req.query.id
+    Nota.findByIdAndUpdate(id, {favorito: true}, {useFindAndModify: false} ,(err, data)=> {
+        if(data){
+            res.sendStatus(200)
+        } else if(err){
+            res.sendStatus(500)
+        } else {
+            res.sendStatus(404)
+        }
+    })
+})
+const port = 3000
+app.listen(port, console.log("Listening on port:", port))
